c79ec0a2b8f2f02197e655015407561e06b85044
Maryam Oluwatosin <61986838+marade1999@users.noreply.github.com>
2020-03-27 11:25:14 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+var txt = "";
+var person = {fname:"maryam", lname:"Adepoju"}; 
+var x;
+for (x in person) {
+  txt += person[x] + " ";
+}
+document.getElementById("demo").innerHTML = txt;
+var courses =["html", "css", "javascript"];
+var x; 
+for(X of courses) { document.write
+</script>
